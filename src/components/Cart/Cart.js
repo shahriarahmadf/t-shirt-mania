e1c4a53eca2cd7c2fc7306c4114ceb425175c959
shahriarahmadf <shahriarahmadf@gmail.com>
2022-10-30 14:43:58 +0900

    connected cart and tshirt select and remove button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({cart, handleRemoveItem}) => {
     return (
         <div>
             <h3>Order Summary</h3>
+            <h5>Order Quantity: {cart.length}</h5>
+            {
+                cart.map(tshirt => 
+                <p key = {tshirt._id}>
+                    {tshirt.name} 
+                        <button onClick={() => handleRemoveItem(tshirt)}>X</button>
+                </p>)
+            }
         </div>
     );
 };
--- src/components/Tshirt/Tshirt.js
+++ src/components/Tshirt/Tshirt.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Tshirt.css'
 
-const Tshirt = ({tshirt}) => {
+const Tshirt = ({tshirt, handleAddToCart}) => {
     const {name, picture, price} = tshirt;
     return (
         <div className='t-shirt'>
             <img src={picture} alt="" />
             <h2>{name}</h2>
             <h4>Price: {price}</h4>
-            <button>Buy This</button>
+            <button onClick={() => handleAddToCart(tshirt)}>Buy This</button>
             
         </div>
     );
