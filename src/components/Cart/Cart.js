b27ad3bd556f1fe5a35f129b015902e771bb0453
shahriarahmadf <shahriarahmadf@gmail.com>
2022-10-30 15:44:27 +0900

    added conditional message on cart and conditional classNames for backgroundColor

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart, handleRemoveItem}) => {
     return (
         <div>
-            <h3>Order Summary</h3>
-            <h5>Order Quantity: {cart.length}</h5>
+            <h3 className={cart.length === 2 ? `orange` : `purple`}>
+                Order Summary
+                </h3>
+            
+            <h5 className={`bold ${cart.length === 2 ? 'yellow' : 'blue'}`}>
+                Order Quantity: {cart.length}
+            </h5>
             {
                 cart.map(tshirt => 
                 <p key = {tshirt._id}>
                     {tshirt.name} 
                         <button onClick={() => handleRemoveItem(tshirt)}>X</button>
                 </p>)
+            }
+            {
+                cart.length > 0 ? <p>Thanks for buying!</p> : <p>Please Buy!</p>
             }
         </div>
     );
